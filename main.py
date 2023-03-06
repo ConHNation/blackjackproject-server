a5befbcb1a2c6b8bf06f1eecce74e3085f75e944
Connor Hayden <51540435+ConHNation@users.noreply.github.com>
2023-03-06 14:39:50 +0000

    Added some comments to main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+# Blackjack - CSP Project
+# Made by Connor Hayden, Dylan Lovell, Mateus and all the other nerds idfk
+
+# dependencies
+from deck import card, newdeck
+
 # acknowledge game start
 print("Starting game...")
